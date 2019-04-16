13b6d020c5452ecc46fa3080383e25b14c685f54
Byambaa <demon.005.gl@gmail.com>
2019-04-16 13:39:42 -0500

    First commit

---
+++ src/main/java/edu/mum/cs/ServletDemo.java
@@ -0,0 +1,51 @@
+package edu.mum.cs;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/calcSum")
+public class ServletDemo extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+            PrintWriter out = response.getWriter();
+
+            out.println("<html><body>");
+            if (request.getParameter("first_sum").equals("") || request.getParameter("second_sum").equals("")) {
+                out.println("<span></span>" + "<br/>");
+            } else {
+                int a = Integer.parseInt(request.getParameter("first_sum"));
+                int b = Integer.parseInt(request.getParameter("second_sum"));
+                int sum = a + b;
+                out.println("<span>" + a + "+" + b + "=" + sum + "</span>" + "<br/>");
+            }
+
+            if (request.getParameter("first_mult").equals("") || request.getParameter("second_mult").equals("")) {
+                out.println("<span></span>" + "<br/>");
+            } else {
+                int c = Integer.parseInt(request.getParameter("first_mult"));
+                int d = Integer.parseInt(request.getParameter("second_mult"));
+                int mult = c * d;
+                out.println("<span>" + c + "*" + d + "=" + mult + "</span>");
+            }
+            out.println("</body></html>");
+
+        } catch (NumberFormatException e) {
+            // log exception and / or notify user
+            System.out.println("At least one invalid number in the given numbers: " + request.getParameter("first_sum") + ", " + request.getParameter("second_sum"));
+            e.printStackTrace();
+            // show an error message to the user somewhere in your frontend
+            response.getWriter().println("<span>error</span>");
+        }
+    }
+}
+
